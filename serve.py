59c43e06a8dcecd132f68346af167a853f4bb2f6
Eric Widengård <eric.widengard@gmail.com>
2025-09-06 18:05:14 +0200

    testing webstie

---
+++ serve.py
@@ -0,0 +1,21 @@
+import http.server
+import socketserver
+import webbrowser
+import threading
+
+PORT = 8000
+DIRECTORY = "."
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+def open_browser():
+    webbrowser.open(f"http://localhost:{PORT}/football_facts.html")
+
+if __name__ == "__main__":
+    threading.Timer(1.5, open_browser).start()
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        httpd.serve_forever()
+   
